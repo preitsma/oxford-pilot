75bbb0b1079c113e429f458adb6ee4c07d84d49d
Peter Reitsma <preitsma@MacBookPro-Peter-Reitsma.local>
2014-03-27 15:01:32 +0100

    finished complete funcionality

--- pronunciation/js/module.js
+++ pronunciation/js/module.js
@@ -19,15 +19,19 @@ angular.module('myApp.service',[]).
 
 angular.module('myApp',['myApp.service','ngDragDrop']);
 
-var AppController = function($scope,$sce,DataSource) {
+var AppController = function($scope,$sce,$timeout,DataSource) {
 
     var SOURCE_FILE = "content/xml/content.xml";
 
     $scope.playing = false;
-    $scope.GroupCounter = 0;
+    $scope.groupCounter = 0;
     $scope.questions = [];
     $scope.options = [];
+    $scope.maxGroups = 0;
+    $scope.finished = false;
     var optionsBackup;
+
+
     
     xmlTransform = function(data) {
         console.log("transform data");
@@ -38,13 +42,14 @@ var AppController = function($scope,$sce,DataSource) {
     };
     
     setData = function(data) {
-        $scope.questionGroup = data.compositequiz.quizzes.quiz.questionGroup[$scope.GroupCounter];
-        
+        $scope.questionGroup = data.compositequiz.quizzes.quiz.questionGroup[$scope.groupCounter];
+        $scope.maxGroups = data.compositequiz.quizzes.quiz.questionGroup.length;
+        console.log($scope.maxGroups);
         angular.forEach($scope.questionGroup.question, function(question) {
-           question.givenAnswer = '     ';
+           //question.givenAnswer = '     ';
            question.answerGiven = false;
            question.status = 'NO_ANSWER'
-           question.answer = null;
+           //question.answer = null;
            console.log(question);
         });
         $scope.options = $scope.questionGroup.options.option;
@@ -54,7 +59,7 @@ var AppController = function($scope,$sce,DataSource) {
         $scope.shownCounter = 0;
     };
 
-    getAnswerId = function(question) {
+    getCorrectAnswerId = function(question) {
         answerId = 0;
         angular.forEach(question.questionItems.item, function(item) {
             if(item._type == 'lozenge') {
@@ -64,12 +69,33 @@ var AppController = function($scope,$sce,DataSource) {
         return answerId;
     }
 
+    getCorrectOption = function(question) {
+        retOption = null;
+        angular.forEach(question.questionItems.item, function(item) {
+            if(item._type == 'lozenge') {
+               answerId = item.answers.answer._answerid
+
+               angular.forEach($scope.options, function(option) {
+                    if (option._optionid ==  item.answers.answer._answerid) {
+                        retOption = option;
+                    }
+               });
+
+            } 
+        });
+        return retOption;
+    }
+
 
     $scope.$on('ANGULAR_DRAG_START', function(e) {$scope.playClip("content/shared_assets/audio/click_high")});
     $scope.$on('ANGULAR_DRAG_END', function(e) {$scope.playClip("content/shared_assets/audio/drop")});
 
-    $scope.playClick = function(e) {
+    $scope.playHighClick = function(e) {
         $scope.playClip("content/shared_assets/audio/click_high");
+    }
+
+    $scope.playLowClick = function(e) {
+        $scope.playClip("content/shared_assets/audio/click_low");
     }
 
     $scope.playDrop = function(e) {
@@ -88,12 +114,15 @@ var AppController = function($scope,$sce,DataSource) {
          }
     };
 
-    // $scope.onDrop = function($event,$answer,$question){
-    //     $question.givenAnswer = $answer.__cdata;
-    //     $question.givenAnswerId = $answer._optionid;
-    //     $question.answerGiven = true;
-    //     $question.status = 'ANSWER_GIVEN'
-    // };
+    $scope.onDrop = function(e, b, index) {
+        //restore the options
+        $scope.options = jQuery.extend(true, {}, optionsBackup);
+
+        $scope.playDrop();
+        question = $scope.questions[index];
+        question.answerGiven = true;
+        question.status = 'ANSWER_GIVEN';
+    }
 
     $scope.tryAgain = function() {
          angular.forEach($scope.questions, function(question) {
@@ -109,7 +138,7 @@ var AppController = function($scope,$sce,DataSource) {
     $scope.checkAnswer = function() {
         allCorrect = true;
         angular.forEach($scope.questions, function(question) {
-            if ( question.givenAnswerId == getAnswerId(question) ) {
+           if ( question.givenAnswer._optionid == getCorrectOption(question)._optionid ) {
                question.status = 'ANSWER_CORRECT';
            } else {
                question.status = 'ANSWER_WRONG';
@@ -118,27 +147,53 @@ var AppController = function($scope,$sce,DataSource) {
         });
         if (allCorrect) {
             $scope.playClip("content/shared_assets/audio/applause")
-            $scope.GroupCounter++;
-            $scope.reset();
+            $scope.skipNext();
         } else {
             $scope.playClip("content/shared_assets/audio/wrong")
         }
     };
 
+    $scope.skipNext = function() {
+        console.log($scope.groupCounter + ' ' + $scope.maxGroups);
+        if( $scope.groupCounter < $scope.maxGroups-1 ) {
+           $timeout(function(e) {
+             $scope.groupCounter++;
+             $scope.reset();
+          }, 3000);
+        } else {
+           $scope.finished = true;
+        }
+    }
+
     $scope.reset = function() {
         DataSource.get(SOURCE_FILE,setData,xmlTransform);
     }
 
-    $scope.showNextAnswer = function() {
+    $scope.restart = function() {
+        $scope.groupCounter = 0;
+        $scope.finished = false;
+        $scope.reset();
+    }
+
+    $scope.seeNextAnswer = function() {
         question = $scope.questions[$scope.shownCounter];
-        angular.forEach($scope.options, function(option) {
-             if (question.questionItems.item[2].answers.answer._answerid == option._optionid) {
-                question.givenAnswer = option.__cdata;
-                question.status = 'ANSWER_SHOWN';  
-                question.answerGiven = true;           }
-        });
+        question.givenAnswer = getCorrectOption(question);
+        question.status = 'ANSWER_SHOWN';  
+        question.answerGiven = true;         
         $scope.shownCounter++;
-        console.log(question);
+        if ($scope.shownCounter == $scope.questions.length) {
+            $scope.skipNext();
+        }
+    }
+
+    $scope.seeAllAnswers = function() {
+        $scope.shownCounter = $scope.questions.length;
+        angular.forEach($scope.questions, function(question) {
+            question.givenAnswer = getCorrectOption(question);
+            question.status = 'ANSWER_SHOWN';  
+            question.answerGiven = true;    
+        });
+        $scope.skipNext();
     }
 
     $scope.isComplete = function() { //all answers are given or correct
@@ -149,7 +204,7 @@ var AppController = function($scope,$sce,DataSource) {
            }
         });
         if (ret == true) {
-            return $scope.shownCounter < 3;
+            return $scope.shownCounter < $scope.questions.length;
         } else {
            return false;
         }
@@ -175,17 +230,6 @@ var AppController = function($scope,$sce,DataSource) {
         return ret;
     }
 
-    $scope.onDrop = function(e, b, index) {
-        //restore the options
-        $scope.options = jQuery.extend(true, {}, optionsBackup);
-
-        $scope.playDrop();
-        question = $scope.questions[index];
-        question.givenAnswer = question.answer.__cdata;
-        question.givenAnswerId = question.answer._optionid;
-        question.answerGiven = true;
-        question.status = 'ANSWER_GIVEN';
-    }
 
     DataSource.get(SOURCE_FILE,setData,xmlTransform);
 
