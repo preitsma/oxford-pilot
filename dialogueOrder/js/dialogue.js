d36684db92f355418a51e65ffd29d4ef3b80a362
Neonila Shlikhtenko <shlikhtenkoneonila@gmail.com>
2014-04-02 13:02:56 +0300

    refactored the code

--- dialogueOrder/js/dialogue.js
+++ dialogueOrder/js/dialogue.js
@@ -8,25 +8,25 @@ var dialogueEntity;
 
 angular.module('orderDialogue',['ngSanitize','ngDragDrop'])
   .factory('Dialogues',
-    ['$http',function($http){ //factory to fetch the content
-       return {
-           getDialogue: function(file,callback, transform){
-                $http.get(
-                    file,
-                    {transformResponse:transform}
-                ).
-                success(function(data, status) {
-                  setData(data);
-                }).
-                error(function(data, status) {
-                    alert('Something went wrong parsing ' + file);
-                });
-           },
-           resetDialogue: function(){
-              return angular.copy(dialogueEntity);
-           }
-       };
-  }])
+    ['$http', function($http) { //factory to fetch the content
+      return {
+        getDialogue: function(file,callback, transform){
+          $http.get(
+              file,
+              {transformResponse:transform}
+          ).
+          success(function(data, status) {
+            setData(data);
+          }).
+          error(function(data, status) {
+              alert('Something went wrong parsing ' + file);
+          });
+        },
+        resetDialogue: function(){
+          return angular.copy(dialogueEntity);
+        }
+      };
+    }])
   .factory('XmlToJson', function() {
     var x2js = new X2JS();
 
@@ -83,30 +83,27 @@ GLOBAL APP CONTROLLER
 
     $scope.onDrop = function(e, b, index) {
         //restore the option that have been removed by d&d module
-        console.log('drop:' + index);
         swapDialoguesByIndexes($scope.start, index);
     }
 
     $scope.onStart = function(e, b, index) {
-        $scope.start = index;
         //restore the option that have been removed by d&d module
-        console.log('start: ' + index);
+        $scope.start = index;
     }
 
     $scope.reset = function(){
-      if(typeof($scope.dialogues)!=="undefined"){
+      if(typeof($scope.dialogues)!=="undefined") {
         $scope.playFile(null,null,'click_low');
         $scope.dialogues = Dialogues.resetDialogue();
-      }else {
+      }
+      else {
         Dialogues.getDialogue(SOURCE_FILE,setData, XmlToJson.convert);
       }
-
 
       $scope.isStarted = false;
       $scope.isChecked = false;
       $scope.allAnswersShown = false;
     };
-
 
     /*
       WORKING WITH SOUNDS - PRELOADING SAMPLES
@@ -114,17 +111,16 @@ GLOBAL APP CONTROLLER
     $scope.sounds = [];
     $scope.playing = false;
 
-
     initSound = function(fileName) {
-        return new buzz.sound(fileName.replace(/\.[^/.]+$/, ""), {
-                    formats: ["ogg", "mp3"]
-                  })
-                  .bind("ended", function(e) { $scope.playing = false; })
-                  .load();
+      return new buzz.sound(fileName.replace(/\.[^/.]+$/, ""), {
+        formats: ["ogg", "mp3"]
+      })
+      .bind("ended", function(e) { $scope.playing = false; })
+      .load();
     }
 
     cacheSound = function(fileName) {
-        $scope.sounds[fileName] = initSound(SOUND_PATH + fileName);
+      $scope.sounds[fileName] = initSound(SOUND_PATH + fileName);
     }
 
     cacheSound('click_low');
@@ -132,13 +128,13 @@ GLOBAL APP CONTROLLER
     cacheSound('wrong');
 
     $scope.playFile = function(a,b,name) {
-        $scope.playSound($scope.sounds[name]);
+      $scope.playSound($scope.sounds[name]);
     }
 
     $scope.playSound = function(sound) {
-       if ($scope.playing == false) {
-            $scope.playing = true;
-            sound.play();
+       if($scope.playing == false) {
+        $scope.playing = true;
+        sound.play();
        }
     }
 
@@ -147,7 +143,7 @@ GLOBAL APP CONTROLLER
       var result;
 
       angular.forEach($scope.dialogues, function(dialogue, index) {
-        if(result == null && dialogue.isCorrect == null){
+        if(result == null && dialogue.isCorrect == null) {
           result = index;
         }
       });
@@ -167,14 +163,10 @@ GLOBAL APP CONTROLLER
     }
 
     function swapDialoguesByIndexes(firstIndex, secondIndex) {
-      $scope.isStarted = true;
+      $scope.isStarted = true; 
+
       var tmp = angular.copy($scope.dialogues[firstIndex]);
-
-      console.log('swapping ' + tmp.text);
-
       $scope.dialogues[firstIndex] = angular.copy($scope.dialogues[secondIndex]);
-
-      console.log('with ' + $scope.dialogues[secondIndex].text);
       $scope.dialogues[secondIndex] = tmp;
     }
 
@@ -184,8 +176,9 @@ GLOBAL APP CONTROLLER
       angular.forEach($scope.dialogues, function(dialogue, index){
         var result = (dialogue.id == index);
         dialogue.isCorrect = result;
-        if(result)numcorrect++;
+        if(result) numcorrect++;
       });
+
       var totalAnswers = $scope.dialogues.length;
       if(numcorrect == totalAnswers) $scope.allAnswersShown = true;
 
@@ -200,9 +193,10 @@ GLOBAL APP CONTROLLER
     };
 
     $scope.tryAgain = function(){
-      $scope.playFile(null,null,'click_low');
+      $scope.playFile(null, null, 'click_low');
       $scope.isChecked = false;
       $scope.isStarted = false;
+
       angular.forEach($scope.dialogues, function(dialogue, index){
         if(dialogue.isCorrect) {
           index = dialogue.id;
@@ -212,9 +206,6 @@ GLOBAL APP CONTROLLER
         }
       });
     };
-
-
-    var cnt = 0;
 
     $scope.seeNextAnswer = function(){
       $scope.playFile(null,null,'click_low');
@@ -252,4 +243,4 @@ GLOBAL APP CONTROLLER
 
     $scope.reset();
 
-  }]);
+}]);
